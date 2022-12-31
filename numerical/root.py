e67080d1789e169fa91a9b8fa92ec6125d87040b
Gettemy, Matthew V (Matthew) <matthewv.gettemy@ge.com>
2022-12-30 22:55:40 -0500

    Added a check to square root to compare against math.sqrt runtimes.

--- numerical/root.py
+++ numerical/root.py
@@ -3,9 +3,16 @@
 """
 
 import math
+import random
+import time
 
 
 def square_root(number, lower, root, rounds):
+    """
+    Less efficient than the built in math.sqrt. the built in algo probably uses
+    interpolation to find a test point closer to the actual root instead of
+    always using the midpoint.
+    """
 
     rounds -= 1
 
@@ -26,7 +33,18 @@ def square_root(number, lower, root, rounds):
 
 
 if __name__ == "__main__":
-    number = 151251
-    rounds = 100
-    root = square_root(number, 1, number, rounds)
-    print(root)
+    bisection_square_root_times = []
+    builtin_square_root_times = []
+
+    ints = [random.randint(1, 1000000) for _ in range(10000)]
+    for i in ints:
+        t0 = time.perf_counter()
+        result = math.sqrt(i)
+        builtin_square_root_times.append(time.perf_counter() - t0)
+
+        t0 = time.perf_counter()
+        result = square_root(i, 1, i, 100)
+        bisection_square_root_times.append(time.perf_counter() - t0)
+
+    print(sum(builtin_square_root_times) / len(builtin_square_root_times))
+    print(sum(bisection_square_root_times) / len(bisection_square_root_times))
